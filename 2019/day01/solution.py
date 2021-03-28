b318f9a7afe08b134c920c8a18bcdfb4dc4d72aa
The Basti <s.rother@iwoca.de>
2021-03-28 11:40:28 +0100

    Adding day 01

---
+++ 2019/day01/solution.py
@@ -0,0 +1,27 @@
+import math
+from utils.SolutionTemplate import SolutionTemplate
+
+def get_fuel_per_weight(weight):
+    fuel_needed = math.floor(weight / 3) - 2
+    if fuel_needed <= 0:
+        return 0
+
+    return fuel_needed + get_fuel_per_weight(fuel_needed)
+
+class Solution(SolutionTemplate):
+    @staticmethod
+    def _clean_input(input):
+        new_input = [int(line) for line in input.split('\n') if line != '']
+        return new_input
+
+    def part_1(self):
+        fuel_per_part = [math.floor(part / 3) - 2 for part in self.data]
+        return sum(fuel_per_part)
+
+    def part_2(self):
+        fuel_per_part = [get_fuel_per_weight(part) for part in self.data]
+        return sum(fuel_per_part)
+
+
+
+
